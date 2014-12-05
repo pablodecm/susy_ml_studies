f9d64a248ec54a6f909f8c01368d38c5d42472dc
pablodecm <pablodecm@gmail.com>
2014-12-05 13:57:39 +0100

    Basic cmake structure, created template TSelector for SUSY ntuples
    Dictionary generation is not automatic yet. An executable has been
    added to test the selector over one ntuple.

---
+++ skimming/src/one_ntuple.cpp
@@ -0,0 +1,9 @@
+
+#include "GeneralSkimmer.h"
+#include "iostream"
+
+int main(int argc, char * argv[])
+{
+    std::cout << "The GeneralSkimmer will be applied to one ntuple" << std::endl;
+    return 0;
+}
